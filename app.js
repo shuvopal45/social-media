089d4740de1916f139719c9ff8d46214e604c8eb
Shuvo <palshuvo13@gmail.com>
2023-07-15 21:50:59 +0600

    six

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use(middleware)
 app.use(router)
 
 const PORT = process.env.PORT || 9090
-const URI = process.env.MONGO_URI
-mongoose.connect(URI, {
+mongoose.connect('mongodb+srv://shuvo:kwI0tkSMq6c9aUnY@cluster0.2pulu.mongodb.net/facebook', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => {
     if (mongoose.connection) {
