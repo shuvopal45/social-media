d112eedc04019afc29de244ff1488a5c34a9b45e
Shuvo <palshuvo13@gmail.com>
2023-08-08 16:08:42 +0600

    j

--- routes/router.js
+++ routes/router.js
@@ -22,7 +22,7 @@ const bcrypt = require('bcrypt')
 
 router.post('/register', async (req, res, next) => {
     try {
-        const { username, pin, image } = req.body
+        const { username, pin, file } = req.body
         console.log(image)
         const findUser = await User.findOne({ username })
         const salt = await bcrypt.genSalt(11)
@@ -31,7 +31,7 @@ router.post('/register', async (req, res, next) => {
             return res.status(400).json('User already exist!')
         } else {
             const user = new User({
-                username, password: hashedPin
+                username, password: hashedPin, profilePic: file
             })
             const saveUser = await user.save()
             if (saveUser) {
