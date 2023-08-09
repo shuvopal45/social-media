b09d9dcb4497d5e9db441243094bf29957f4cc33
Shuvo <palshuvo13@gmail.com>
2023-08-09 21:14:44 +0600

    '32132432423434324'

--- middleware/checkLogin.js
+++ middleware/checkLogin.js
@@ -4,7 +4,7 @@ const checkLogin = (req, res, next) => {
     const { authorization } = req.headers
     try {
         const token = authorization.split(' ')[1]
-        const decode = jwt.verify(token, process.env.SECRET_KEY)
+        const decode = jwt.verify(token, 'ahsdgashgdjsaghasygfashgfasjkhgfksgfasfgsafgasjgasjhgasjfga76tuy')
         const { username, userId } = decode
         req.username = username
         req.userId = userId
